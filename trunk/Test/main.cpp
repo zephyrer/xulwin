e4f5cd7c716d2567dcca4c206724b801e0f66000
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2009-10-10 18:57:36 +0000

    Remove Test project from XULWin solution. Create Test solution and add XULWin project to it.

--- trunk/Test/main.cpp
+++ trunk/Test/main.cpp
@@ -39,16 +39,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     ErrorReporter::Instance().setLogger(boost::bind(&log, _1));
 
-    //runConfigSample();
+    runConfigSample();
     runImageViewerSample();
-    //XULWin::Tester tester;
-    //tester.runXULSample("hello");
-    //tester.runXULSample("toolbar");
-    //tester.runXULSample("widgets");
-    //tester.runXULSample("tabbox");
-    //tester.runXULSample("treeview");
-    //tester.runXULSample("configpanel");
-    //tester.runXULSample("shout");
-    //tester.runXULSample("svg");
+    XULWin::Tester tester;
+    tester.runXULSample("hello");
+    tester.runXULSample("toolbar");
+    tester.runXULSample("widgets");
+    tester.runXULSample("tabbox");
+    tester.runXULSample("treeview");
+    tester.runXULSample("configpanel");
+    tester.runXULSample("shout");
+    tester.runXULSample("svg");
     return 0;
 }

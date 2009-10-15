f6fdf5e4fcbb9652845f679f6bd33c88c94700a2
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2009-10-15 14:29:11 +0000

    Element creation script now also updates the Visual Studio project.

--- trunk/XULWin/scripts/create-el.rb
+++ trunk/XULWin/scripts/create-el.rb
@@ -1,16 +1,33 @@
-hppdir = ARGV[0]
-cppdir = ARGV[1]
-classname = ARGV[2]
+require "rexml/document"
+
+projectpath = '../'
+hppdir = projectpath + 'include/XULWin/'
+cppdir = projectpath + 'src/'
+
+classname = ARGV[0]
+projectname = "XULWin_vs80.vcproj"
 
 puts "Classname: " + classname
 puts "Element name: " + classname.downcase()
 puts "hppdir: " + hppdir
 puts "cppdir: " + cppdir
 
+hppfile = classname + ".h"
+hpppath = hppdir + hppfile
+
+cppfile = classname + ".cpp"
+cpppath = cppdir + cppfile
+
+hppimplfile = classname + "Impl.h"
+hppimplpath = hppdir + hppimplfile
+
+cppimplfile = classname + "Impl.cpp"
+cppimplpath = cppdir + cppimplfile
+
 
 outFile = "test.h"
 File.open("ElementTemplate.h", 'r') do |elHeaderTemplate|
-    File.open(hppdir + classname + ".h", 'w') do |out|
+    File.open(hpppath, 'w') do |out|
         while (line = elHeaderTemplate.gets())
             out << line.gsub("{{ELEMENT_NAME}}", classname).gsub(
                              "{{ELEMENT_NAME_UPPER}}", classname.upcase()).gsub(
@@ -20,7 +37,7 @@ File.open("ElementTemplate.h", 'r') do |elHeaderTemplate|
 end
 
 File.open("ElementTemplate.cpp", 'r') do |elCppTemplate|
-    File.open(cppdir + classname + ".cpp", 'w') do |out|
+    File.open(cpppath, 'w') do |out|
         while (line = elCppTemplate.gets())
             out << line.gsub("{{ELEMENT_NAME}}", classname)
         end
@@ -28,7 +45,7 @@ File.open("ElementTemplate.cpp", 'r') do |elCppTemplate|
 end
 
 File.open("ElementImplTemplate.h", 'r') do |template|
-    File.open(hppdir + classname + "Impl.h", 'w') do |out|
+    File.open(hppimplpath, 'w') do |out|
         while (line = template.gets())
             out << line.gsub("{{ELEMENTIMPL_NAME}}", classname + "Impl").gsub(
                              "{{ELEMENTIMPL_NAME_UPPER}}", (classname + "Impl").upcase())
@@ -37,10 +54,41 @@ File.open("ElementImplTemplate.h", 'r') do |template|
 end
 
 File.open("ElementImplTemplate.cpp", 'r') do |template|
-    File.open(cppdir + classname + "Impl.cpp", 'w') do |out|
+    File.open(cppimplpath, 'w') do |out|
         while (line = template.gets())
             out << line.gsub("{{ELEMENTIMPL_NAME}}", classname + "Impl");
         end
     end
+end
+
+
+contents = ""
+File.open(projectpath + projectname, 'r') do |file|
+    doc = REXML::Document.new file
+    firstFile = doc.elements["VisualStudioProject/Files/Filter/"]
+    puts firstFile
+
+    headerElement = REXML::Element.new("File")
+    headerElement.attributes["RelativePath"] = ".\\include\\XULWin\\" + hppfile;
+    firstFile.insert_after(firstFile, headerElement)
+
+    headerElement = REXML::Element.new("File")
+    headerElement.attributes["RelativePath"] = ".\\src\\" + cppfile;
+    firstFile.insert_after(firstFile, headerElement)
+
+    headerElement = REXML::Element.new("File")
+    headerElement.attributes["RelativePath"] = ".\\include\\XULWin\\" + hppimplfile;
+    firstFile.insert_after(firstFile, headerElement)
+
+    headerElement = REXML::Element.new("File")
+    headerElement.attributes["RelativePath"] = ".\\src\\" + cppimplfile;
+    firstFile.insert_after(firstFile, headerElement)
+
+    contents << doc.to_s
+    contents.sub!(/'8,00'/, "\"8,00\"")
+end
+
+File.open(projectpath + projectname, 'w') do |file|
+    file << contents
 end
 

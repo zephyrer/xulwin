d153b4910b3e34d3440d5d3f692d329658b8bde3
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2009-10-15 21:54:58 +0000

    Shizzle :S

---
+++ trunk/Scripts/CreateElement/ElementImplTemplate.h
@@ -0,0 +1,30 @@
+#ifndef {{ELEMENTIMPL_NAME_UPPER}}_H_INCLUDED
+#define {{ELEMENTIMPL_NAME_UPPER}}_H_INCLUDED
+
+
+#include "XULWin/ElementImpl.h"
+
+
+namespace XULWin
+{
+
+    class {{ELEMENTIMPL_NAME}} : public PassiveComponent
+    {
+    public:
+        typedef PassiveComponent Super;
+
+        {{ELEMENTIMPL_NAME}}(ElementImpl * inParent, const AttributesMapping & inAttributesMapping);
+
+        virtual bool initImpl();
+
+        bool initAttributeControllers();
+
+        int calculateWidth(SizeConstraint inSizeConstraint) const;
+
+        int calculateHeight(SizeConstraint inSizeConstraint) const;
+    };
+
+} // namespace XULWin
+
+
+#endif // {{ELEMENTIMPL_NAME_UPPER}}_H_INCLUDED

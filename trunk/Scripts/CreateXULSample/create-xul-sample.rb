bdb66f4105db147779c52ba91caa4b90f81c176e
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2009-10-15 21:30:53 +0000

    create-xul-sample.rb works now.

--- trunk/Scripts/CreateXULSample/create-xul-sample.rb
+++ trunk/Scripts/CreateXULSample/create-xul-sample.rb
@@ -1,16 +1,46 @@
 require "rexml/document"
 
 appName = ARGV[0]
-srcPath = "CreateXULSample/"
-targetPath = "../../xulrunnersamples/#{appName}"
+srcPath = "XULSampleTemplate/"
+targetPath = "../../xulrunnersamples/#{appName}/"
 system("svn export #{srcPath} #{targetPath}")
 
-File.open(srcPath + "application.ini", 'r') do |template|
-    File.open(targetPath + "application.ini", 'w') do |out|
+applicationIniFile = "application.ini"
+File.open(srcPath + applicationIniFile, 'r') do |template|
+    File.open(targetPath + applicationIniFile, 'w') do |out|
         while (line = template.gets())
             out << line.gsub("{{APP_NAME}}", appName).gsub(
-                             "{{APP_NAME_DOWNCASE}}", classname.downcase())
+                             "{{APP_NAME_DOWNCASE}}", appName.downcase())
         end
     end
 end
 
+manifestFile = "chrome/chrome.manifest"
+File.open(srcPath + manifestFile, 'r') do |template|
+    File.open(targetPath + manifestFile, 'w') do |out|
+        while (line = template.gets())
+            out << line.gsub("{{APP_NAME}}", appName).gsub(
+                             "{{APP_NAME_DOWNCASE}}", appName.downcase())
+        end
+    end
+end
+
+xulFile = "chrome/content/main.xul"
+File.open(srcPath + xulFile, 'r') do |template|
+    File.open(targetPath + xulFile, 'w') do |out|
+        while (line = template.gets())
+            out << line.gsub("{{APP_NAME}}", appName).gsub(
+                             "{{APP_NAME_DOWNCASE}}", appName.downcase())
+        end
+    end
+end
+
+prefsFile = "defaults/preferences/prefs.js"
+File.open(srcPath + prefsFile, 'r') do |template|
+    File.open(targetPath + prefsFile, 'w') do |out|
+        while (line = template.gets())
+            out << line.gsub("{{APP_NAME}}", appName).gsub(
+                             "{{APP_NAME_DOWNCASE}}", appName.downcase())
+        end
+    end
+end

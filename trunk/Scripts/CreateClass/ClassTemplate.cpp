8fa90710d300f747e07d5c27ec191c559404a241
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2009-10-19 20:41:21 +0000

    Add CreateClass script.

---
+++ trunk/Scripts/CreateClass/ClassTemplate.cpp
@@ -0,0 +1,16 @@
+#include "XULWin/{{CLASS_NAME}}.h"
+
+
+namespace XULWin
+{
+
+    {{CLASS_NAME}}::{{CLASS_NAME}}()
+    {
+    }
+    
+    
+    {{CLASS_NAME}}::~{{CLASS_NAME}}()
+    {
+    }
+
+} // namespace XULWin
---
+++ trunk/Scripts/CreateClass/ClassTemplate.h
@@ -0,0 +1,19 @@
+#ifndef {{CLASS_NAME_UPPER}}_H_INCLUDED
+#define {{CLASS_NAME_UPPER}}_H_INCLUDED
+
+
+namespace XULWin
+{
+
+    class {{CLASS_NAME}}
+    {
+    public:
+        {{CLASS_NAME}}();
+
+        virtual ~{{CLASS_NAME}}();
+    };
+
+} // namespace XULWin
+
+
+#endif // {{CLASS_NAME_UPPER}}_H_INCLUDED

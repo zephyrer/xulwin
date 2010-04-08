d3ef2d1f6101498c5114fa74dedb8fbb1ad341cc
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2010-04-08 19:16:02 +0000

    Don't break brackets in header files.

--- Branches/Main_DOMElementRefactoring/3rdParty/CodeFormatter/FormatCode.bat
+++ Branches/Main_DOMElementRefactoring/3rdParty/CodeFormatter/FormatCode.bat
@@ -1,1 +1,4 @@
-astyle  --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --indent-col1-comments --max-instatement-indent=80 --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --recursive *.h *.cpp 
+set COMMON_OPTIONS=--indent=spaces=4 --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --indent-col1-comments --max-instatement-indent=80 --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --recursive
+astyle --brackets=break $COMMON_OPTIONS *.h
+astyle $COMMON_OPTIONS *.cpp
+

9bac06dd9f24c6c4b6a7d30965bbdf75bce9161e
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2010-06-03 19:31:02 +0000

    Formatted Component.h. Update formatter scripts.

---
+++ trunk/3rdParty/CodeFormatter/FormatCpp.bat
@@ -0,0 +1,10 @@
+@echo off
+if "%1" == "" goto error
+
+3rdParty\CodeFormatter\astyle  --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --indent-col1-comments --max-instatement-indent=80 --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --recursive "%1"
+goto end
+
+:error
+echo "Usage FormatCpp.bat CPPFile"
+
+:end

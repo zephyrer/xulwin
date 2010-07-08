dc7eb49136be29f366e76f43f60f9cb060a7a189
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2010-07-08 18:19:46 +0000

    Add FacebookUploader sample. Replace ErrorHandler with exceptions in XULRunner.cpp.

---
+++ trunk/Samples/FacebookUploader/main.cpp
@@ -0,0 +1,73 @@
+#include "XULWin/Component.h"
+#include "XULWin/Decorator.h"
+#include "XULWin/ErrorReporter.h"
+#include "XULWin/Initializer.h"
+#include "XULWin/Unicode.h"
+#include "XULWin/Window.h"
+#include "XULWin/XMLWindow.h"
+#include "XULWin/WinUtils.h"
+#include "XULWin/XULRunner.h"
+#include "Poco/File.h"
+#include <sstream>
+#include <string>
+
+
+using namespace XULWin;
+
+
+std::string unquote(const std::string & inString)
+{
+    if (inString.size() <= 2)
+    {
+        return inString;
+    }
+
+    if ((inString[0] == '"' && inString[inString.size() - 1] == '"') ||
+            (inString[0] == '\'' && inString[inString.size() - 1] == '\''))
+    {
+        return inString.substr(1, inString.size() - 2);
+    }
+    return inString;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Initialize the XULWin library
+    Initializer initializer(hInstance);
+
+    std::string xulPath = unquote(lpCmdLine);
+    if (xulPath.empty())
+    {
+        xulPath = "facebook-uploader";
+    }
+
+    if (!Poco::File(xulPath).exists())
+    {
+        std::wstring msg = L"Could not find " + ToUTF16(xulPath) + L".";
+        MessageBox(0, msg.c_str(), L"Facebook Uploader", MB_OK);
+        return 1;
+    }
+
+    try
+    {
+        WinAPI::CurrentDirectoryChanger cd(xulPath);
+        XULRunner xulRunner(hInstance);
+        ErrorCatcher errorCatcher;
+        xulRunner.loadApplication("application.ini");
+        ElementPtr root = xulRunner.rootElement();
+        Window * window = root->component()->downcast<Window>();
+        if (!window)
+        {
+            throw std::runtime_error("Root element is not a window.");
+        }
+        window->showModal(WindowPos_CenterInScreen);
+    }
+    catch (const std::exception & inException)
+    {
+        std::wstring msg = ToUTF16(inException.what());
+        MessageBox(0, msg.c_str(), L"Facebook Uploader", MB_OK);
+        return 1;
+    }
+    return 0;
+}

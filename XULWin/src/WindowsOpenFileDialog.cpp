124b121ae8c2363ed643dcf177621453787a9d97
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2010-07-09 23:03:03 +0000

    The image filter for WindowsOpenFileDialog works fine now.

--- XULWin/src/WindowsOpenFileDialog.cpp
+++ XULWin/src/WindowsOpenFileDialog.cpp
@@ -10,73 +10,87 @@
 
 extern "C"
 {
-    
-    unsigned int CALLBACK OpenFileDialogHook(HWND hWnd, UINT inMsg, WPARAM wParam, LPARAM lParam)
-    {
-        switch (inMsg)
-        {
-	        case WM_INITDIALOG:
-	        {
-		        // You need to use a copy of the OPENFILENAME struct used to
-		        // create this dialog. You can store a pointer to the
-		        // OPENFILENAME struct in the ofn.lCustData so you can
-		        // retrieve it here in the lParam. Once you have it, you
-		        // need to hang on to it. Using window properties provides a
-		        // good thread safe solution to using a global variable.
-		        if(!SetProp(::GetParent(hWnd), _T("OFN"), (void *) lParam))
-		        {
-                    XULWin::ReportError("SetProp failed. Last error: " + XULWin::WinAPI::getLastError(::GetLastError()));
-		        }
-		        return 0;
-	        }
-	        case WM_NOTIFY:
-	        {
-		        // The OFNOTIFY struct is passed in the lParam of this message.
-		        LPOFNOTIFY ofNotify = (LPOFNOTIFY) lParam;
-		        switch (ofNotify->hdr.code)
-		        {
-			        case CDN_SELCHANGE:
-			        {				        				        
-                        XULWin::UInt32 bufferSize = CommDlg_OpenSave_GetSpec(::GetParent(hWnd), NULL, 0) + MAX_PATH;
+	
+    unsigned int CALLBACK DialogHook(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		//static HWND hwndParentDialog;
+		LPOFNOTIFY lpofn;
+		int cbLength;
+		static LPTSTR lpsz;
+		//static int LastLen;
 
-				        // The OFN struct is stored in a property of dialog window
-                        LPOPENFILENAME ofNotify = (LPOPENFILENAME)::GetProp(::GetParent(hWnd), TEXT("OFN"));
+		switch (uMsg)
+		{
+			case WM_INITDIALOG:
+			{
+				// You need to use a copy of the OPENFILENAME struct used to
+				// create this dialog. You can store a pointer to the
+				// OPENFILENAME struct in the ofn.lCustData so you can
+				// retrieve it here in the lParam. Once you have it, you
+				// need to hang on to it. Using window properties provides a
+				// good thread safe solution to using a global variable.
+				if(!SetProp(GetParent(hwnd), _T("OFN"), (void *) lParam))
+				{
+					MessageBox(NULL, _T("SET PRop Failed"), _T("ERROR"), MB_OK);
+				}
+				return (0);
+			}
+			case WM_COMMAND:
+			{
+				OutputDebugString(_T("command\n"));
+				break;
+			}
+			case WM_NOTIFY:
+			{
+				// The OFNOTIFY struct is passed in the lParam of this
+				// message.
+				lpofn = (LPOFNOTIFY) lParam;
 
-				        if (ofNotify->nMaxFile < static_cast<DWORD>(bufferSize))
-				        {
-                            static LPTSTR buffer(0);
+				switch (lpofn->hdr.code)
+				{
+					case CDN_SELCHANGE:
+					{
+						LPOPENFILENAME lpofn;
+						cbLength = CommDlg_OpenSave_GetSpec(GetParent(hwnd), NULL, 0);
 
-					        // Free the previously allocated buffer.
-					        if(buffer)
-					        {
-						        HeapFree(GetProcessHeap(), 0, buffer);
-                                buffer = 0;
-					        }
+						cbLength += _MAX_PATH;
 
-					        // Allocate a new buffer
-					        buffer = (LPTSTR) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, bufferSize);
-					        if (buffer)
-					        {
-						        ofNotify->lpstrFile = buffer;
-						        ofNotify->nMaxFile = bufferSize;
-					        }
-				        }
-				        break;
-			        }
-		        }
-		        break;
-	        }
-	        case WM_DESTROY:
-	        {
-		        // Also need to free the property with the OPENFILENAME struct.
-		        ::RemoveProp(::GetParent(hWnd), TEXT("OFN"));
-		        break;
-            }
-        }
-        return 0;
-    }
+						// The OFN struct is stored in a property of dialog window
+						lpofn = (LPOPENFILENAME) GetProp(GetParent(hwnd), _T("OFN"));
 
-} // extern 'C'
+						if (int(lpofn->nMaxFile) < cbLength)
+						{
+							// Free the previously allocated buffer.
+							if(lpsz)
+							{
+								HeapFree(GetProcessHeap(), 0, lpsz);
+							}
+							// Allocate a new buffer
+							lpsz = (LPTSTR) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, cbLength*sizeof(_T('\0')));
+							if (lpsz)
+							{
+								lpofn->lpstrFile = lpsz;
+								lpofn->nMaxFile  = cbLength;
+							}
+						}
+						break;
+					}
+				}
+				break;
+			}
+			case WM_DESTROY:
+			{
+				// Also need to free the property with the OPENFILENAME
+				// struct.
+				RemoveProp(GetParent(hwnd), _T("OFN"));
+				break;
+			 }
+		}
+		return (0);
+	}
+
+} // extern "C"
+
 
 
 namespace XULWin
@@ -85,33 +99,30 @@ namespace XULWin
     namespace WinAPI
     {
 
-        const std::string & GetImagesFilter()
+        LPCWSTR GetImagesFilter()
         {
-            static const std::string cFilter = 
-                "All supported\0"
-                "*.jpg;*.jpeg;*.bmp;*.png;*.gif;*.tif;*.tiff\0"
-                "JPG\0*.jpg;*.jpeg\0"
-                "BMP\0*.bmp\0"
-                "PNG\0*.png\0"
-                "GIF\0*.gif\0"
-                "TIFF\0*.tif;*.tiff\0";
-            return cFilter;
+            return L"All supported\0"
+                   L"*.jpg;*.jpeg;*.bmp;*.png;*.gif;*.tif;*.tiff\0"
+                   L"JPG\0*.jpg;*.jpeg\0"
+                   L"BMP\0*.bmp\0"
+                   L"PNG\0*.png\0"
+                   L"GIF\0*.gif\0"
+                   L"TIFF\0*.tif;*.tiff\0";
         }
 
 
-        void ChooseFile(HWND mParent,
+        void ChooseFile(HWND inParentWindow,
                         const std::string & inDialogTitle,
-                        const std::string & inFilter,
+                        LPCWSTR inFilter,
                         bool inAllowMultiSelect,
                         std::vector<std::string> & outSelectedFiles)
         {
+	        // Allocate a new buffer
 	        OPENFILENAME openFileName;
-            ::ZeroMemory(&openFileName, sizeof(openFileName));
+	        ZeroMemory(&openFileName, sizeof(openFileName));
 	        openFileName.lStructSize = sizeof(openFileName);
-	        openFileName.hwndOwner = mParent;
-        	
-            std::wstring utf16Filter = ToUTF16(inFilter);
-            openFileName.lpstrFilter = utf16Filter.c_str();
+	        openFileName.hwndOwner = inParentWindow;
+            openFileName.lpstrFilter = inFilter;
 	        openFileName.nFilterIndex = 0;
 	        openFileName.lpstrFile = 0;
 	        openFileName.nMaxFile = MAX_PATH;
@@ -120,38 +131,37 @@ namespace XULWin
             {
                 openFileName.Flags |= OFN_ALLOWMULTISELECT;
             }
-	        openFileName.lpfnHook = OpenFileDialogHook;
+	        openFileName.lpfnHook = DialogHook;
         	
-            if(::GetOpenFileName(&openFileName))
+	        if(GetOpenFileName(&openFileName))
 	        {
 		        // Split the string into filenames
-		        for (WCHAR * offset = openFileName.lpstrFile + openFileName.nFileOffset;
-			         *offset;
-			         offset += (wcslen(offset) + 1))
+		        for (WCHAR * szTemp = openFileName.lpstrFile + openFileName.nFileOffset;
+			         *szTemp;
+			         szTemp += (wcslen(szTemp) + 1))
 		        {
-			        if(openFileName.lpstrFile[wcslen(openFileName.lpstrFile) + 4] == L'\0')
+			        if(openFileName.lpstrFile[ wcslen(openFileName.lpstrFile) + 4 ] == L'\0')
 			        {
 				        outSelectedFiles.push_back(ToUTF8(openFileName.lpstrFile));
 			        }
 			        else
 			        {
-				        size_t dwLen = wcslen(openFileName.lpstrFile) + wcslen(offset) + 2;
-				        WCHAR * buffer = new WCHAR[dwLen];
-				        wcscpy_s(buffer, dwLen, openFileName.lpstrFile);
-				        wcscat_s(buffer, dwLen, L"\\");
-				        wcscat_s(buffer, dwLen, offset);
-				        outSelectedFiles.push_back(ToUTF8(buffer));
-				        delete buffer;
+				        size_t dwLen = wcslen(openFileName.lpstrFile) + wcslen(szTemp) + 2;
+				        WCHAR * szFile = new WCHAR[dwLen];
+				        wcscpy_s(szFile, dwLen, openFileName.lpstrFile);
+				        wcscat_s(szFile, dwLen, L"\\");
+				        wcscat_s(szFile, dwLen, szTemp);
+				        outSelectedFiles.push_back(ToUTF8(szFile));
+				        delete szFile;
 			        }
 		        }
 	        }
 	        else
 	        {
-		        DWORD errorCode = CommDlgExtendedError();
-                ReportError("Failed call to GetOpenFileName with error: " + WinAPI::getLastError(errorCode));
+		        DWORD theErrorCode = CommDlgExtendedError();
 	        }
-        }
 
+        }
 
     } // namespace WinAPI
 

743b5b6857f9f5de3106cc8aa146a8b129ef2c30
francis.rammeloo <francis.rammeloo@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2010-07-09 23:03:03 +0000

    The image filter for WindowsOpenFileDialog works fine now.

--- trunk/XULWin/include/XULWin/WindowsOpenFileDialog.h
+++ trunk/XULWin/include/XULWin/WindowsOpenFileDialog.h
@@ -13,31 +13,34 @@ namespace XULWin
     namespace WinAPI
     {
 
-        const std::string & GetImagesFilter();
+        /**
+         * Filter for image files (jpg, jpeg, bmp, png, gif, tif, tiff)
+         */
+        LPCWSTR GetImagesFilter();
 
         /**
          * Opens a file chooser dialog.
          *
          * Example filter:
-         *   "All supported\0"
-         *   "*.jpg;*.jpeg;*.bmp;*.png;*.gif;*.tif;*.tiff;*.3gp;*.avi;*.divx;*.flv;*.mov;*.mpeg;*.mpg;*.mp4;*.rm;*.wmv\0"
-         *   "JPG\0*.jpg;*.jpeg\0"
-         *   "BMP\0*.bmp\0"
-         *   "PNG\0*.png\0"
-         *   "GIF\0*.gif\0"
-         *   "TIFF\0*.tif;*.tiff\0"
-         *   "3GP\0*.3gp;*.3GP\0"
-         *   "AVI\0*.avi;*.AVI\0"
-         *   "DivX\0*.divx;*.DIVX\0"
-         *   "FLV\0*.flv;*.FLV\0"
-         *   "MOV\0*.mov;*.MOV\0"
-         *   "MPEG\0*.mpg;*.MPG;*.mpeg;*.MPEG;*.mp4;*.MP4\0"
-         *   "RM\0*.rm;*.RM\0"
-         *   "WMV\0*.wmv;*.WMV\0";
+         *   L"All supported\0"
+         *   L"*.jpg;*.jpeg;*.bmp;*.png;*.gif;*.tif;*.tiff;*.3gp;*.avi;*.divx;*.flv;*.mov;*.mpeg;*.mpg;*.mp4;*.rm;*.wmv\0"
+         *   L"JPG\0*.jpg;*.jpeg\0"
+         *   L"BMP\0*.bmp\0"
+         *   L"PNG\0*.png\0"
+         *   L"GIF\0*.gif\0"
+         *   L"TIFF\0*.tif;*.tiff\0"
+         *   L"3GP\0*.3gp;*.3GP\0"
+         *   L"AVI\0*.avi;*.AVI\0"
+         *   L"DivX\0*.divx;*.DIVX\0"
+         *   L"FLV\0*.flv;*.FLV\0"
+         *   L"MOV\0*.mov;*.MOV\0"
+         *   L"MPEG\0*.mpg;*.MPG;*.mpeg;*.MPEG;*.mp4;*.MP4\0"
+         *   L"RM\0*.rm;*.RM\0"
+         *   L"WMV\0*.wmv;*.WMV\0";
          */
         void ChooseFile(HWND mParent,
                         const std::string & inDialogTitle,
-                        const std::string & inFilter,
+                        LPCWSTR inFilter,
                         bool inAllowMultiSelect,
                         std::vector<std::string> & outSelectedFiles);
 

db823b6d3f5670955c7e142e74133a4ebdb2c80f
francis.rammeloo@gmail.com <francis.rammeloo@gmail.com@2ec99694-a20f-11de-ba4d-5dbaeb624ccf>
2011-07-29 22:17:14 +0000

    Cleanum formatting

--- trunk/XULWin/include/XULWin/Defaults.h
+++ trunk/XULWin/include/XULWin/Defaults.h
@@ -5,205 +5,238 @@
 #include "XULWin/Enums.h"
 
 
-namespace XULWin
+namespace XULWin {
+namespace Defaults {
+
+
+static std::string defaultLocale()
 {
+    return "en-US";
+}
 
-    namespace Defaults
-    {
-        static std::string defaultLocale()
-        {
-            return "en-US";
-        }
 
-        static int windowWidth()
-        {
-            return 320;
-        }
+static int windowWidth()
+{
+    return 320;
+}
 
-        static int windowHeight()
-        {
-            return 240;
-        }
 
-        static int menuBarHeight()
-        {
-            return 23;
-        }
+static int windowHeight()
+{
+    return 240;
+}
 
-        static int menuBarSpacing()
-        {
-            return 2;
-        }
 
-        static int controlHeight()
-        {
-            return 21;
-        }
+static int menuBarHeight()
+{
+    return 23;
+}
 
-        // default control width is rarely used since
-        // we usually calculate the minimum width
-        static int controlWidth()
-        {
-            return 40;
-        }
 
-        static int buttonHeight()
-        {
-            return 23;
-        }
+static int menuBarSpacing()
+{
+    return 2;
+}
 
-        static int buttonWidth()
-        {
-            return 75;
-        }
 
-        static int tabHeight()
-        {
-            return 23;
-        }
+static int controlHeight()
+{
+    return 21;
+}
 
-        static int textPadding()
-        {
-            return 12;
-        }
 
-        static int menuListMinWidth()
-        {
-            return 24;
-        }
+// default control width is rarely used since
+// we usually calculate the minimum width
+static int controlWidth()
+{
+    return 40;
+}
 
-        static int dropDownListItemHeight()
-        {
-            return 16;
-        }
 
-        static int componentMinimumWidth()
-        {
-            return 80;
-        }
+static int buttonHeight()
+{
+    return 23;
+}
 
-        static int componentMinimumHeight()
-        {
-            return 20;
-        }
 
-        static int radioButtonMinimumWidth()
-        {
-            return 20;
-        }
+static int buttonWidth()
+{
+    return 75;
+}
 
-        static int textBoxWidth()
-        {
-            return 20;
-        }
 
-        static int textBoxMinimumWidth()
-        {
-            return textPadding();
-        }
+static int tabHeight()
+{
+    return 23;
+}
 
-        static int spinButtonWithoutTextBox()
-        {
-            return 16;
-        }
 
-        static int checkBoxMinimumWidth()
-        {
-            return 20;
-        }
+static int textPadding()
+{
+    return 12;
+}
 
-        static int scrollbarWidth()
-        {
-            return 17;
-        }
 
-        static int progressMeterWidth()
-        {
-            return 125;
-        }
+static int menuListMinWidth()
+{
+    return 24;
+}
 
-        static int progressMeterHeight()
-        {
-            return 17;
-        }
 
-        static int treeIndent()
-        {
-            return 18;
-        }
+static int dropDownListItemHeight()
+{
+    return 16;
+}
 
-        static int treeItemHeight()
-        {
-            return 16;
-        }
 
-        static int statusBarHeight()
-        {
-            return 23;
-        }
+static int componentMinimumWidth()
+{
+    return 80;
+}
 
-        static int toolbarHeight()
-        {
-            return 23;
-        }
 
-        static int listViewColumnTextPadding()
-        {
-            return 15;
-        }
+static int componentMinimumHeight()
+{
+    return 20;
+}
 
-        namespace Attributes
-        {
-            static bool disabled()
-            {
-                return false;
-            }
 
-            static bool readonly()
-            {
-                return false;
-            }
+static int radioButtonMinimumWidth()
+{
+    return 20;
+}
 
-            static bool checked()
-            {
-                return false;    // checkbox
-            }
 
-            static Orient orient()
-            {
-                return Vertical;
-            }
+static int textBoxWidth()
+{
+    return 20;
+}
 
-            static Align align()
-            {
-                return Stretch;
-            }
 
-            static int flex()
-            {
-                return 0;
-            }
+static int textBoxMinimumWidth()
+{
+    return textPadding();
+}
 
-            static int pageincrement()
-            {
-                return 10;
-            }
 
-            static int curpos()
-            {
-                return 0;
-            }
+static int spinButtonWithoutTextBox()
+{
+    return 16;
+}
 
-            static int maxpos()
-            {
-                return 100;
-            }
 
-        } // namespace Attributes
+static int checkBoxMinimumWidth()
+{
+    return 20;
+}
 
-    } // namespace Defaults
 
-} // namespace XULWin
+static int scrollbarWidth()
+{
+    return 17;
+}
+
+
+static int progressMeterWidth()
+{
+    return 125;
+}
+
+
+static int progressMeterHeight()
+{
+    return 17;
+}
+
+
+static int treeIndent()
+{
+    return 18;
+}
+
+
+static int treeItemHeight()
+{
+    return 16;
+}
+
+
+static int statusBarHeight()
+{
+    return 23;
+}
+
+
+static int toolbarHeight()
+{
+    return 23;
+}
+
+
+static int listViewColumnTextPadding()
+{
+    return 15;
+}
+
+
+namespace Attributes {
+
+
+static bool disabled()
+{
+    return false;
+}
+
+
+static bool readonly()
+{
+    return false;
+}
+
+
+static bool checked()
+{
+    return false;    // checkbox
+}
+
+
+static Orient orient()
+{
+    return Vertical;
+}
+
+
+static Align align()
+{
+    return Stretch;
+}
+
+
+static int flex()
+{
+    return 0;
+}
+
+
+static int pageincrement()
+{
+    return 10;
+}
+
+
+static int curpos()
+{
+    return 0;
+}
+
+
+static int maxpos()
+{
+    return 100;
+}
+
+
+} } } // namespace XULWin::Defaults::Attributes
 
 
 #endif // DEFAULTS_H_INCLUDED
